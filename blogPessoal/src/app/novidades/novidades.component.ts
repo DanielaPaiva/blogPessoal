7cd2d98fbf545f75100eb6c984e095b2a47f497e
unknown <danielapaiva386@gmail.com>
2020-08-26 20:31:13 -0300

    Rotas

---
+++ blogPessoal/src/app/novidades/novidades.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novidades',
+  templateUrl: './novidades.component.html',
+  styleUrls: ['./novidades.component.css']
+})
+export class NovidadesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+  }
+
+}
