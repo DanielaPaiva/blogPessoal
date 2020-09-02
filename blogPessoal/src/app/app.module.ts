7f9ff03c78c6c3c21a80ca796dd145e7e5e97ebd
unknown <danielapaiva386@gmail.com>
2020-09-02 17:11:48 -0300

    Método post

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {OrderModule} from 'ngx-order-pipe';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +16,7 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
 import { NovidadesComponent } from './novidades/novidades.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { PostTemaComponent } from './post-tema/post-tema.component';
 
 @NgModule({
   declarations: [
@@ -26,14 +28,16 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     PerfilLateralComponent,
     NovidadesComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    PostTemaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- blogPessoal/src/app/service/tema.service.ts
+++ blogPessoal/src/app/service/tema.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { Tema } from '../model/Tema';
 
 
 @Injectable({
@@ -18,5 +19,8 @@ export class TemaService {
 
   getByIdTema(id:number){
     return this.http.get(`http://localhost:8080/temas/{id}`,this.token)
+}
+postTema(tema:Tema){
+  return this.http.post('http://localhost:8080/temas' ,tema,this.token)
 }
 }
